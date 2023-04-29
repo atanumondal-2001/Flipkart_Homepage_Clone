87bf6a5caf320ba07f2a5f14391472a7c8cb3a1e
ATANU MONDAL <mondalatanu756@gmail.com>
2023-04-29 20:42:13 +0530

    Updated

--- flipkart.js
+++ flipkart.js
@@ -2,3 +2,15 @@ function changeMode() {
     let myContainer = document.getElementById("MainBody");
     myContainer.classList.toggle('DarkMode')
 }
+
+
+// For Coupons
+function loadCoupon() {
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('allElements').style.opacity = '0.5'
+}
+
+function closeCoupon() {
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('allElements').style.opacity = '1'
+}
