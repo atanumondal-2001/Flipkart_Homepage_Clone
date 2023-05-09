2f5c5569adbc5d327108ae618df7d3ed775cbd31
ATANU MONDAL <mondalatanu756@gmail.com>
2023-05-09 13:21:07 +0530

    Updated with Geo location

---
+++ Geolocation.js
@@ -0,0 +1,39 @@
+let locButton = document.getElementById("loc_btn");
+let locOutput = document.getElementById("locationOut");
+
+locButton.addEventListener("click", () => {
+    //inbuild js function to get location latitude and longitude
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showLoc, checkError);
+    } else {
+        locOutput.innerText = "Browser Not Supported";
+    }
+});
+
+//Error Checking....
+const checkError = () => {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            locOutput.innerText = "Allow access to Location";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            locOutput.innerText = "Location Unavailable";
+            break;
+        case error.TIMEOUT:
+            locOutput.innerText = "Time-Out!!"
+    }
+};
+
+const showLoc = async(position) => {
+    let response = await fetch(
+        `https://nominatim.openstreetmap.org/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`
+    );
+
+    let data = await response.json();
+    console.log(data.address.state_district);
+    console.log(data.address.state);
+    console.log(data.address.country_code);
+
+    locOutput.innerHTML = `<p class=OutPut>${data.address.state_district}, ${data.address.state}, ${data.address.country_code}</p>`;
+}
